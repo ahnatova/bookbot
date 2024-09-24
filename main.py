78ffe8ef26b35443df0890464d46f101e5a0b6fa
ahnatova <a.hnatova@seznam.cz>
2024-09-24 17:13:15 +0200

    added function for counting characters in a text

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ def main():
     text = get_book_text(book_path)
     number_of_words = get_num_words(text)
     print(f"The number of words is: {number_of_words}")
+    number_of_characters = get_num_chararacters(text)
+    print(number_of_characters)
+
+def get_num_chararacters(text):
+    lowered_text = text.lower()
+    characters = {}
+    for c in lowered_text:
+        if c in characters:
+            characters[c] += 1
+        else:
+            characters[c] = 1
+        
+    return characters
+
 
 def get_num_words(text):
     words = text.split()
